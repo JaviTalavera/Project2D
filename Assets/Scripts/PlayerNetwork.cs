ddd9955a15ba57b13a1c622e74fd0daea7bc89f9
Javier Talavera Moya <javi.tm90@gmail.com>
2016-05-12 23:57:51 +0200

    Primera subida

---
+++ Assets/Scripts/PlayerNetwork.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class PlayerNetwork : NetworkBehaviour {
+
+	public override void OnStartLocalPlayer ()
+	{
+		GetComponent<Player> ().enabled = true;
+		GetComponent<Controller2D> ().enabled = true;
+	}
+}
