6e90ad55183d5462bf29b6a4ccdf5c49ca34c8ef
Javier Talavera Moya <javi.tm90@gmail.com>
2016-05-14 14:35:13 +0200

    Nuevo personaje de fuego

---
+++ Assets/Notas.js
@@ -0,0 +1,27 @@
+﻿/*== When spawning
+  NetCorrector nc = go.GetComponent<NetCorrector>();
+  nc.rotation = go.transform.rotation;
+  nc.scale = go.transform.localScale;
+  NetworkServer.Spawn(go);
+
+
+== To Update position/scaling
+
+using UnityEngine;
+using UnityEngine.Networking;
+using System;
+
+public class NetCorrector : NetworkBehaviour {
+    [SyncVar]
+    public Vector3 scale;
+    [SyncVar]
+    public Quaternion rotation;
+    [SyncVar]
+    public string objName;
+
+    void Start () {
+        transform.localScale = scale;
+        transform.rotation = rotation;
+        gameObject.name = objName;
+    }
+}*/
