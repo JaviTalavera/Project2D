860020b4f7d8a9cba5e466ce8d521a93457de040
Javier Talavera Moya <javi.tm90@gmail.com>
2016-06-09 23:25:47 +0200

    Nuevo enemigo

---
+++ Assets/EnemyHandle.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyHandle : MonoBehaviour {
+
+    public float timeBetweenAttacks = 3f;
+    public GameObject projectile;
+    private int nPlayers = 0;
+    public Transform thrower;
+
+    private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public IEnumerator Attack ()
+    {
+        while (true)
+        {
+            anim.SetTrigger("Attack");
+            GameObject go = Instantiate(projectile, thrower.position, projectile.transform.rotation) as GameObject;
+            go.GetComponent<Rigidbody2D>().AddForce(100*(Vector2.up+Vector2.right*Mathf.Sign(transform.localScale.x)));
+            yield return new WaitForSeconds(timeBetweenAttacks);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag.Contains("Player"))
+        {
+            if (nPlayers == 0)
+                StartCoroutine("Attack");
+            nPlayers++;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag.Contains("Player"))
+        {
+            nPlayers--;
+            if (nPlayers == 0)
+                StopAllCoroutines();
+        }
+    }
+}
---
+++ Assets/RockHandle.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RockHandle : MonoBehaviour {
+
+    private Rigidbody2D rb;
+    public float force;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+
+    void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Obstacle"))
+        {
+            Vector2 v = rb.velocity;
+            v.y = 0;
+            rb.velocity = v;
+            rb.AddForce(force * Vector2.up);
+        }
+        else if (other.gameObject.tag.Contains("Player"))
+        {
+            other.gameObject.GetComponent<PlayerMovement>().doDamage(50f);
+            Destroy(gameObject);
+        }
+    }
+}
